d62a01908b5a2a830b6f519ba963a5c7952262d3
l7rf1i82 <msdo24ws@gmail.com>
2022-04-10 21:20:16 +0600

    init

---
+++ exec.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# MacoOS
+
+export PHP_PURE_CODE="var_dump('php cli app exec !');"
+
+php -r "$PHP_PURE_CODE"
---
+++ main.php
@@ -0,0 +1,10 @@
+$main_var = 2048;
+
+
+function main()
+{
+	return "exec function main";
+}
+
+echo $main_var . "\n";
+echo main() . "\n";
